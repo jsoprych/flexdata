993148334f5101828cc8562bb6caad00e919cfbd
John Soprych <johnsoprych@gmail.com>
2025-07-14 21:21:59 -0500

    Update build.js to bundle new core components

--- build.js
+++ build.js
@@ -1,11 +1,18 @@
+// build.js
 const esbuild = require('esbuild');
 
 esbuild.build({
-    entryPoints: ['src/components/fd-grid.ts', 'src/components/fd-detail-view.ts', 'src/data-controller.ts'],
-    outdir: 'public/assets/js',
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    target: ['es2020', 'chrome58', 'firefox57', 'safari11'],
-    format: 'iife'
+  entryPoints: [
+    'src/core/components/FDGrid.ts',
+    'src/core/components/FDDetailView.ts',
+    'src/core/components/FDStatusBar.ts',
+    'src/core/BaseControl.ts',
+    'src/data-controller.ts'
+  ],
+  outdir: 'public/assets/js',
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  target: ['es2020', 'chrome58', 'firefox57', 'safari11'],
+  format: 'iife'
 }).then(() => console.log('Build succeeded')).catch(() => process.exit(1));
